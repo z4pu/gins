6e449fe905fa5e7347ca66a88e4cb634387a0429
szp <luminasce@gmail.com>
2017-03-28 14:13:24 +0800

    scraper start

--- sample/sample.py
+++ sample/sample.py
@@ -2,13 +2,44 @@
 #!/usr/bin/python3
 
 import helpers
+import requests
+from lxml import html
+from beautifulsoup4 import BeautifulSoup
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
+# URLS for Goethe Institut pages
+sites = {'exam': 'https://www.goethe.de/ins/sg/en/spr/prf/anm.html' ,
+        'course': 'https://www.goethe.de/ins/sg/en/spr/kur/gia/tup.cfm',
+        'testDaF': 'http://www.testdaf.de/fuer-teilnehmende/die-pruefung/pruefungstermine/',
+        'registrationInfo' : 'https://www.goethe.de/ins/sg/en/spr/kur/gia/kue.html'}
+
+def get_info(address):
+    page = requests.get(address)
+    if page.status_code == 200:
+        tree = html.fromstring(page.content)
+        print("Tree success!")
+        return tree
+    elif 100 <= page.status_code <200:
+        print(page.status_code)
+    elif 200 < page.status_code < 400:
+        print(page.status_code)
+    else:
+        page.raise_for_status()
 
 
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+def get_course():
+    page = requests.get('https://www.goethe.de/ins/sg/en/spr/kur/gia/tup.cfm')
+    return
+
+def get_testDaF():
+    page = requests.get('http://www.testdaf.de/fuer-teilnehmende/die-pruefung/pruefungstermine/')
+    return
+
+def get_registrationInfo():
+    page = requests.get('https://www.goethe.de/ins/sg/en/spr/kur/gia/kue.html')
+    return
+
+def main():
+    for k, v in sites.items():
+        get_info(v)
+
+main()
